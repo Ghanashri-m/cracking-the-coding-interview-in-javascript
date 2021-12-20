a17dafdf49729cad14c92a9c8901308a83f81814
Ghanashri M <ghanashri.28@gmail.com>
2021-12-20 15:55:35 -0800

    Update is-pallindrome-permutation.js

--- ch-01-arrays-and-strings/is-pallindrome-permutation.js
+++ ch-01-arrays-and-strings/is-pallindrome-permutation.js
@@ -1,2 +1,25 @@
 // Determine whether or not a string is a permutation of a palindrome,
 // ignoring spaces.
+
+const canBePallindrome = (input) => {
+  let str = input.toLowerCase().split(" ").join("");
+  let charMap = {};
+  let hasOddCount = false;
+  
+  for (let char of str) {
+  	charMap[char] = charMap[char] ? charMap[char] + 1 : 1
+  }
+  
+  for (let char in charMap) {
+    if (charMap[char] % 2 !== 0) {
+    	if (hasOddCount) {
+      	console.log("not a permutation of a pallindrome");
+        return;
+      }
+      hasOddCount = true;
+    }
+  }
+  console.log("String is a permutation of a pallindrome")
+}
+
+canBePallindrome(str="Tact Coaa")
