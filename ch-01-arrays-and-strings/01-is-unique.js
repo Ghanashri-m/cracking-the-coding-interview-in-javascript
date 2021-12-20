ef89157572dfb9019d8925b54e9e1b29d44f3aef
Ghanashri M <ghanashri.28@gmail.com>
2021-12-20 13:46:37 -0800

    Update 01-is-unique.js

--- ch-01-arrays-and-strings/01-is-unique.js
+++ ch-01-arrays-and-strings/01-is-unique.js
@@ -1,1 +1,15 @@
-console.log("Hello world")
+// Determine whether or not a given string contains no duplicate characters.
+
+const checkDuplicate = (testString) => {
+  
+	let outcome = testString.toLowerCase().split('').every((item, index, arr) => arr.indexOf(item) === arr.lastIndexOf(item));
+ 
+  if (outcome) {
+  	console.log('String contains no duplicate')
+  } else {
+    console.log('String contins duplicates')
+  }
+  
+}
+let str = 'String';
+checkDuplicate(str);
