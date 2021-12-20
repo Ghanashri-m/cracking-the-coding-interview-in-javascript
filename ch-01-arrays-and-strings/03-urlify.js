fb80bb2b97f1775070233198a92bc81da9696360
Ghanashri M <ghanashri.28@gmail.com>
2021-12-20 14:39:57 -0800

    Update 03-urlify.js

--- ch-01-arrays-and-strings/03-urlify.js
+++ ch-01-arrays-and-strings/03-urlify.js
@@ -1,15 +1,10 @@
 // Replace spaces in the middle of a string with "%20" assuming the end of the 
 // string contains twice as many spaces as are in the middle.
 
+// example: input: "Mr John Smith    "; output: "Mr%20John%20Smith"
+
 const urlify = (input) => {
-  let spaceCount = input.split(" ").length - 1;
-  let outcome = input.split(" ").join("%20");
-  
-  for (let i = 0; i < (spaceCount * 2); i++) {
-  	outcome += '%20';
-  }
-  
-  console.log(outcome);
+  console.log(input.trim().split(" ").join("%20"));
 }
 
-urlify(str="Hello World!!")
+urlify(str="Mr John Smith    ")
